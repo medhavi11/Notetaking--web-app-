f5ae38fe86188eb31362196b02e2a3d9b6ef25bd
Medhavi Basera <1905781@kiit.ac.in>
2021-06-04 17:29:54 +0530

    initial files added

---
+++ App.js
@@ -0,0 +1,8 @@
+const validator = require("validator");
+const chalk = require("chalk");
+console.log(chalk.green.bold.inverse("Success"));
+// const notes = require("./Notes");
+
+// const text = notes();
+// console.log(validator.isEmail("Medhavi@gmail.com"));
+// console.log(validator.isURL("Medhavi@gmail"));
